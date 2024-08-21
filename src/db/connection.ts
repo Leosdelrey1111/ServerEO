39d2c23dab2c148a2818c395c366aee6a6466f57
Leonel Alejandro Torres Pérez <leyotorres1501@gmail.com>
2024-08-21 17:28:12 -0600

    Lo que faltaba de todo el proyecto

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -6,7 +6,7 @@ import { Sequelize } from 'sequelize';
     logging: false
   }); */
 
-  const sequelize = new Sequelize('mr_macondo', 'root', 'celeste1234', {
+  const sequelize = new Sequelize('mr_macondo', 'root', '123456789', {
     dialect: 'mysql',
     logging: false
   });
